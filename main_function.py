4a237311167c1a36e6a8f742b538e141123941f5
smileysmith82 <160980503+smileysmith82@users.noreply.github.com>
2024-03-05 08:47:40 -0700

    Update main_function.py

--- main_function.py
+++ main_function.py
@@ -3,6 +3,22 @@ import random
 import copy
 
 
+class Card():
+    def __init__(self,rank,suit):
+        self.rank=0
+        self.suit= ''
+        self.selected = False
+
+
+        if self.suit == 'C':
+            self.suit = 'circle'
+        elif self.suit = 'S':
+            self.suit = 'square'
+        elif self.suit == 'T':
+            self.suit= "triangle"
+    
+
+
 class Deck():
     def __init__(self):
-    
+        self.Deck = []
