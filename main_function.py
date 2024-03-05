18771a2d8f3cd6885cc8aabdc47d281d65d09dea
smileysmith82 <160980503+smileysmith82@users.noreply.github.com>
2024-03-05 09:22:42 -0700

    Update main_function.py
    I added the Deck Function and a test case for it

--- main_function.py
+++ main_function.py
@@ -5,20 +5,41 @@ import copy
 
 class Card():
     def __init__(self,rank,suit):
-        self.rank=0
-        self.suit= ''
+        self.rank= rank
+        self.suit= suit
         self.selected = False
 
 
         if self.suit == 'C':
-            self.suit = 'circle'
-        elif self.suit = 'S':
-            self.suit = 'square'
+            self.suit_name = 'circle'
+        elif self.suit == 'S':
+            self.suit_name = 'square'
         elif self.suit == 'T':
-            self.suit= "triangle"
+            self.suit_name = "triangle"
+            
+    def __str__(self):
+        
+        if self.rank == 0:
+            print("Sylops")
+        else:
+            return f"{self.rank} of {self.suit}"
     
 
 
 class Deck():
     def __init__(self):
-        self.Deck = []
+        self.deck = []
+        self.suits = ["C","S","T"]
+        for suit in self.suits:
+            for rank in range (1,11):
+                self.deck.append(Card(rank,suit))
+        for i in range (2):
+            self.deck.append(Card(0, ''))
+    def print_deck(self):
+        for card in self.deck:
+            print(card)
+            
+deck = Deck()
+deck.print_deck()        
+
+
