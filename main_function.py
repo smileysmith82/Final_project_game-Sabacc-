58ca12aebce0fa5bed60c2664eca9cbae7b418d3
smileysmith82 <160980503+smileysmith82@users.noreply.github.com>
2024-03-05 09:44:54 -0700

    Update main_function.py
    Finalized the deck

--- main_function.py
+++ main_function.py
@@ -5,7 +5,7 @@ import copy
 
 class Card():
     def __init__(self,rank,suit):
-        self.rank= rank
+        self.rank= int(rank)
         self.suit= suit
         self.selected = False
 
@@ -20,7 +20,7 @@ class Card():
     def __str__(self):
         
         if self.rank == 0:
-            print("Sylops")
+            return("Sylops")
         else:
             return f"{self.rank} of {self.suit}"
     
@@ -33,13 +33,19 @@ class Deck():
         for suit in self.suits:
             for rank in range (1,11):
                 self.deck.append(Card(rank,suit))
+                self.deck.append(Card(-rank,suit))
         for i in range (2):
             self.deck.append(Card(0, ''))
     def print_deck(self):
         for card in self.deck:
             print(card)
             
+    def get_deck_length(self):
+        return len(self.deck)
+        
+            
 deck = Deck()
 deck.print_deck()        
 
 
+print(f"Length of Deck:", deck.get_deck_length())
