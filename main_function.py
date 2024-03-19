4afd9598dcc11144f27f4b3ee27231b776654a92
smileysmith82 <160980503+smileysmith82@users.noreply.github.com>
2024-03-19 09:56:01 -0600

    Update main_function.py

--- main_function.py
+++ main_function.py
@@ -7,3 +7,25 @@ WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Sabacc")
 
+BG = pygame.transform.scale(pygame.image.load("Green_background.jpg"), (WIDTH,HEIGHT))
+
+def draw():
+    WIN.blit(BG, (0,0))
+    pygame.display.update()
+
+def main():
+    run = True
+    
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+        
+        draw()
+            
+    pygame.quit()
+    
+    
+if __name__ == "__main__":
+    main()
