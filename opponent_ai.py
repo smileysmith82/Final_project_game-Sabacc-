17e95770a1863e1ab90eea34fc0294f7a6b39916
smileysmith82 <160980503+smileysmith82@users.noreply.github.com>
2024-04-30 11:08:56 -0600

    Update opponent_ai.py

--- opponent_ai.py
+++ opponent_ai.py
@@ -1,3 +1,40 @@
 import math
 import random
-import __from main_function.py
+import Deck_and_Hand
+            
+            
+class AI_Player():
+    
+    def calculate_total(self, hand):
+        total = Deck_and_Hand.total_player2(hand)
+        return total
+
+    def check_if_stand(self):
+        if self.calculate_total() == 0:
+            return True
+        else:
+            return False
+
+    def check_if_switch(self, discard_top):
+        total = self.calculate_total()
+        if discard_top is not None:
+            for card in self.hand:
+                new_total = (total - card.rank)
+                if new_total + discard_top.rank <= abs(1):
+                    return True
+        return False
+
+    def make_move(self, discard_top, draw_pile):
+        if self.check_if_stand():
+            Deck_and_Hand.stand()
+        elif self.check_if_switch(discard_top):
+            switch()
+        else:
+            Deck_and_Hand.draw()
+
+            
+            
+            
+
+
+
